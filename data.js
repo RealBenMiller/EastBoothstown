a3902557b42964a66706beacc29b78e037b05a7c
Ben Miller <ben@benrmiller.co.uk>
2023-01-04 17:00:09 +0000

    updating virtual exhibition

--- data.js
+++ data.js
@@ -1,105 +1,105 @@
 var APP_DATA = {
-  "scenes": [
-    {
-      "id": "0-updated-virtual-exhibition---boothstown",
-      "name": "Updated Virtual Exhibition - Boothstown",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        }
-      ],
-      "faceSize": 1000,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [],
-      "infoHotspots": [
-        {
-          "yaw": 1.0196804277617257,
-          "pitch": -0.1318909868293172,
-          "title": "1",
-          "text": "Text"
-        },
-        {
-          "yaw": 1.4623287363275201,
-          "pitch": -0.1529237130881107,
-          "title": "2",
-          "text": "Text"
-        },
-        {
-          "yaw": 1.9403529131859836,
-          "pitch": -0.140319972975945,
-          "title": "3",
-          "text": "Text"
-        },
-        {
-          "yaw": 2.4590154872878127,
-          "pitch": -0.1352431457825496,
-          "title": "4",
-          "text": "Text"
-        },
-        {
-          "yaw": 2.7469820541893704,
-          "pitch": -0.12644960857051757,
-          "title": "Title",
-          "text": "Text"
-        },
-        {
-          "yaw": -3.1391975755190664,
-          "pitch": -0.13035431593801405,
-          "title": "6",
-          "text": "Text"
-        },
-        {
-          "yaw": -2.7698537288065204,
-          "pitch": -0.13433470306021889,
-          "title": "7",
-          "text": "Text"
-        },
-        {
-          "yaw": -2.4744526123507917,
-          "pitch": -0.128848151342293,
-          "title": "8",
-          "text": "Text"
-        },
-        {
-          "yaw": -1.6812155788347738,
-          "pitch": -0.1594358294608469,
-          "title": "9",
-          "text": "Text"
-        },
-        {
-          "yaw": -1.166707046131208,
-          "pitch": -0.14748043405189115,
-          "title": "10",
-          "text": "Text"
-        },
-        {
-          "yaw": -0.8747674247279171,
-          "pitch": -0.12640137630415538,
-          "title": "11",
-          "text": "Text"
-        }
-      ]
-    }
-  ],
-  "name": "Project Title",
-  "settings": {
-    "mouseViewMode": "drag",
-    "autorotateEnabled": true,
-    "fullscreenButton": false,
-    "viewControlButtons": false
-  }
+	scenes: [
+		{
+			id: '0-updated-virtual-exhibition---boothstown',
+			name: 'Updated Virtual Exhibition - Boothstown',
+			levels: [
+				{
+					tileSize: 256,
+					size: 256,
+					fallbackOnly: true,
+				},
+				{
+					tileSize: 512,
+					size: 512,
+				},
+				{
+					tileSize: 512,
+					size: 1024,
+				},
+			],
+			faceSize: 1000,
+			initialViewParameters: {
+				pitch: 0,
+				yaw: 0,
+				fov: 1.5707963267948966,
+			},
+			linkHotspots: [],
+			infoHotspots: [
+				{
+					yaw: 1.0196804277617257,
+					pitch: -0.1318909868293172,
+					title: '1',
+					text: 'Text',
+				},
+				{
+					yaw: 1.4623287363275201,
+					pitch: -0.1529237130881107,
+					title: '2',
+					text: 'Text',
+				},
+				{
+					yaw: 1.9403529131859836,
+					pitch: -0.140319972975945,
+					title: '3',
+					text: 'Text',
+				},
+				{
+					yaw: 2.4590154872878127,
+					pitch: -0.1352431457825496,
+					title: '4',
+					text: 'Text',
+				},
+				{
+					yaw: 2.7469820541893704,
+					pitch: -0.12644960857051757,
+					title: '5',
+					text: 'Text',
+				},
+				{
+					yaw: -3.1391975755190664,
+					pitch: -0.13035431593801405,
+					title: '6',
+					text: 'Text',
+				},
+				{
+					yaw: -2.7698537288065204,
+					pitch: -0.13433470306021889,
+					title: '7',
+					text: 'Text',
+				},
+				{
+					yaw: -2.4744526123507917,
+					pitch: -0.128848151342293,
+					title: '8',
+					text: 'Text',
+				},
+				{
+					yaw: -1.6812155788347738,
+					pitch: -0.1594358294608469,
+					title: '9',
+					text: 'Text',
+				},
+				{
+					yaw: -1.166707046131208,
+					pitch: -0.14748043405189115,
+					title: '10',
+					text: 'Text',
+				},
+				{
+					yaw: -0.8747674247279171,
+					pitch: -0.12640137630415538,
+					title: '11',
+					text: 'Text',
+				},
+			],
+		},
+	],
+	name: 'Project Title',
+	settings: {
+		mouseViewMode: 'drag',
+		autorotateEnabled: true,
+		fullscreenButton: false,
+		viewControlButtons: false,
+	},
 };
